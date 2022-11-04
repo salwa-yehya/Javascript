56869a25c0044fb58aa6ecb6436e85350c65e5d7
unknown <salwayehyaabuafash@gmail.com>
2022-11-04 12:02:48 -0700

    java

---
+++ Thu_3_nov/task3.js
@@ -0,0 +1,26 @@
+let mark = prompt ( ' enter your mark please !');
+
+if  (mark >= 90 )
+{
+    document.write('A+');  
+}
+else if  (mark >= 80 )
+{
+    document.write('A');  
+}
+else if  (mark >= 70 )
+{ 
+    document.write('B');  
+}
+else if  (mark >= 60 )
+{
+    document.write('C');  
+}
+else if  (mark >= 50 )
+{
+    document.write('D');  
+}
+ else if( mark < 50)
+{
+ document.write('Fail');
+ }
---
+++ Thu_3_nov/task4.js
@@ -0,0 +1,4 @@
+const PI = 3.14 ;
+x = 12* PI **2 ;
+document.write(x);
+console.log(x);
