1755c5f22db1595f58d8fe184b9c17965a0433f0
unknown <salwayehyaabuafash@gmail.com>
2022-11-06 07:10:31 -0800

    java

---
+++ Sun_6_nov/task1.js
@@ -0,0 +1,10 @@
+let x = prompt('enter your birthday month ')
+switch(x)
+{
+    case '8':
+    console.log( 'August ')
+    break;
+    case '3':
+    console.log( 'march ')
+    break;
+}
---
+++ Sun_6_nov/task3.js
@@ -0,0 +1,14 @@
+for(let i=0 ; i < 1000 ; i++ )
+{
+    let x= i+1;
+    let y=x%13;
+    if(y==0)
+    {
+        console.log(x);
+    
+    }
+    else
+    {
+        console.log(`not found`);
+    }
+}
